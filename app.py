0a5eeab91abdd4558783b6331647f69f3c7b368f
Beckett Freeman <71795010+beackers@users.noreply.github.com>
2025-09-28 13:48:48 -0500

    changed to inputting fn into page

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import markdown
 import os
 
 app = Flask(__name__)
 
-@app.route("/view/<mdPage>")
-def view(mdPage):
-	# Define the path to your Markdown fil
+@app.route("/view", methods=['GET'])
+def view():
+	return render_template("view.html")
 
+@app.route("/view", methods=["POST"])
+def viewPage():
+	data = request.json
+	filename = data['filename']
 	try:
-		page = f"{mdPage}.md"
-		with open(page, 'r', encoding='utf-8') as f:
-			markdown_text = f.read()
-		html_content = markdown.markdown(markdown_text)
-		return render_template('view.html', content=html_content)
+		with open(filename) as file:
+			text = file.read()
 	except FileNotFoundError:
-		print("exception caught! filenotfound error")
-		return f"Markdown file not found. Page: {page}"
+		return 404
+	mdText = markdown(text)
+	return jsonify({"file": mdText})
+
 
 if __name__ == '__main__':
 	app.run(debug=True)

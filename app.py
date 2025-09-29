346c7f65713f2e8b55f11f2d4edb45b4f318b851
Beckett Freeman <71795010+beackers@users.noreply.github.com>
2025-09-29 06:47:46 -0500

    Working re-implementation
    Unfortunately I have to thank a stack of servers for helping me find all
    the bugs in the new version. Thanks ChatGPT. Your processors found the
    semicolon I could not.

--- app.py
+++ app.py
@@ -6,22 +6,23 @@ app = Flask(__name__)
 
 @app.route("/view", methods=['GET'])
 def view():
-	return render_template("view.html")
+    return render_template("view.html")
 
 @app.route("/view", methods=["POST"])
 def viewPage():
-	data = request.json
-	filename = data['filename']
-	try:
-		with open(filename) as file:
-			text = file.read()
-	except FileNotFoundError:
-		return jsonify({
+    data = request.json
+    filename = data['filename']
+    try:
+        with open(filename) as file:
+            text = file.read()
+    except FileNotFoundError:
+        print("FileNotFoundError", f"File: {filename}")
+        return jsonify({
             "text": "filenotfound"
             }), 404
-	mdText = markdown(text)
-	return jsonify({"text": mdText})
+    mdText = markdown.markdown(text)
+    return jsonify({"text": mdText}), 200
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
